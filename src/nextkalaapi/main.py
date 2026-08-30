f2bae695107ad43fcdc4cc4ca357ec6b9e52b4c4
khosraviTech <khosravitech@gmail.com>
2026-08-30 15:49:26 +0330

    add user_repository.

--- src/nextkalaapi/main.py
+++ src/nextkalaapi/main.py
@@ -6,12 +6,11 @@ from nextkalaapi.routers import orders, products, users
 app = FastAPI()
 add_scalar_reference(app)
 
-app.include_router(users.router,prefix='/users',tags=["user"])
-app.include_router(orders.router ,prefix='/orders',tags=["order"])
-app.include_router(products.router ,prefix='/products',tags=["product"])
+app.include_router(users.router, prefix="/users", tags=["user"])
+app.include_router(orders.router, prefix="/orders", tags=["order"])
+app.include_router(products.router, prefix="/products", tags=["product"])
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "nextkala API"}
-
-
---
+++ src/nextkalaapi/repositories/user_repository.py
@@ -0,0 +1,50 @@
+
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from nextkalaapi.models.user_model import User
+
+
+# read
+def get_user(db: Session, user_id: int) -> User | None:
+    stmt = select(User).where(User.id == user_id)
+    return db.scalar(stmt)
+
+
+def get_users(db: Session) -> list[User]:
+    stmt = select(User)
+    return list(db.scalars(stmt).all())
+
+
+# create
+def create_user(db: Session, user: User) -> User:
+    db.add(user)
+    db.flush()
+    db.commit()
+    db.refresh(user)
+
+    return user
+
+
+# update
+def update_user(
+    db: Session, user: User
+) -> (
+    User
+):  # changes will happen in Service layer: user_repository.update_user(db, user)
+    db.refresh(user)
+    db.commit()
+    return user
+
+
+# delete
+def delete_user(db: Session, user_id: int) -> int | None:
+    user = get_user(db, user_id)
+
+    if user is None:
+        return None
+
+    db.delete(user)
+    db.commit()
+
+    return user_id
