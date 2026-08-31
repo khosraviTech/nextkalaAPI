8452b4d6b7372366bced17d83041dddb20992c10
khosraviTech <khosravitech@gmail.com>
2026-08-31 07:45:01 +0330

    add order_repository

---
+++ src/nextkalaapi/repositories/order_repository.py
@@ -0,0 +1,47 @@
+from psycopg.errors import SerializationFailure
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from nextkalaapi.models.order_model import Order
+
+
+# read
+def get_order(db: Session, order_id: int) -> Order | None:
+    stmt = select(Order).where(Order.id == order_id)
+    return db.scalar(stmt)
+
+
+def get_orders(db: Session) -> list[Order] | None:
+    stmt = select(Order)
+    return list(db.scalars(stmt).all())
+
+
+# create
+def create_order(db: Session, order: Order) -> Order:
+    db.add(order)
+    db.flush()
+    db.commit()
+    db.refresh(order)
+
+    return order
+
+
+# update
+def update_order(db: Session, order: Order) -> Order:
+    db.refresh(order)
+    db.commit()
+
+    return order
+
+
+# delete
+def delete_order(db: Session, order_id: int) -> int | None:
+    order = get_order(db, order_id)
+
+    if order is None:
+        return None
+    
+    db.delete(order)
+    db.commit()
+
+    return order_id
