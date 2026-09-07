f429fc513f984b6c5c51ddb42953a8254183c0f5
khosraviTech <khosravitech@gmail.com>
2026-09-07 18:24:01 +0330

    add post product router.

--- src/nextkalaapi/routers/products.py
+++ src/nextkalaapi/routers/products.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy import null
 from sqlalchemy.orm import Session
 
 from nextkalaapi.database import get_db
-from nextkalaapi.schemas.product import ProductRow
+from nextkalaapi.schemas.product import ProductInsert, ProductRow
 from nextkalaapi.services import product_service
 
 router = APIRouter()
@@ -14,11 +14,20 @@ router = APIRouter()
 def read_products(db: Session = Depends(get_db)):
     products = product_service.get_products(db)
     return products
+
+
 # read by id
-@router.get("/product_id/{product_id}",response_model=ProductRow)
-def read_product(
-    product_id:int,
-    db:Session=Depends(get_db)
-):
-    return product_service.get_product(db,product_id)
+@router.get("/product_id/{product_id}", response_model=ProductRow)
+def read_product(product_id: int, db: Session = Depends(get_db)):
+    return product_service.get_product(db, product_id)
+
+
+# create
+@router.post(
+    "/create",
+    response_model=ProductRow,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_product(product_data: ProductInsert, db: Session = Depends(get_db)):
+    return product_service.create_product(db, product_data)
 
