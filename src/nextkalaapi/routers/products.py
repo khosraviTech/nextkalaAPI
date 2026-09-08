82d3a0109de1ada7da1daf03fc1f845f5a7763e5
khosraviTech <khosravitech@gmail.com>
2026-09-08 10:05:51 +0330

    add product patch router.

--- src/nextkalaapi/routers/products.py
+++ src/nextkalaapi/routers/products.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy import null
 from sqlalchemy.orm import Session
 
 from nextkalaapi.database import get_db
-from nextkalaapi.schemas.product import ProductDelete, ProductInsert, ProductRow
+from nextkalaapi.schemas.product import (
+    ProductDelete,
+    ProductInsert,
+    ProductRow,
+    ProductUpdate,
+)
 from nextkalaapi.services import product_service
 
 router = APIRouter()
@@ -32,15 +36,28 @@ def create_product(product_data: ProductInsert, db: Session = Depends(get_db)):
     return product_service.create_product(db, product_data)
 
 
-# delete
-@router.delete("/delete/product_id/{product_id}", response_model=ProductDelete)
-def delete_product(product_id: int, db: Session = Depends(get_db)):
-    product = product_service.delete_product(db,product_id)
+# update
+@router.patch("/update/product_id/{product_id}", response_model=ProductRow)
+def update_product(
+    product_id: int, product_data: ProductUpdate, db: Session = Depends(get_db)
+):
+    product = product_service.update_product(db,product_id,product_data)
     if product is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Product by id {product_id} not found",
         )
-  
-    
+    return product
+
+
+# delete
+@router.delete("/delete/product_id/{product_id}", response_model=ProductDelete)
+def delete_product(product_id: int, db: Session = Depends(get_db)):
+    product = product_service.delete_product(db, product_id)
+    if product is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Product by id {product_id} not found",
+        )
+
     return {"id": product}
