6cacbe48c32498484671d448fcdd96652b3855bb
khosraviTech <khosravitech@gmail.com>
2026-09-11 11:42:58 +0330

    define carts router.

--- src/nextkalaapi/main.py
+++ src/nextkalaapi/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from fastapi import FastAPI
 from scalar_fastapi import add_scalar_reference
-from nextkalaapi.routers import orders, products, users
+
+from nextkalaapi.routers import carts, orders, products, users
 
 app = FastAPI()
 add_scalar_reference(app)
@@ -9,6 +9,8 @@ add_scalar_reference(app)
 app.include_router(users.router, prefix="/users", tags=["user"])
 app.include_router(orders.router, prefix="/orders", tags=["order"])
 app.include_router(products.router, prefix="/products", tags=["product"])
+app.include_router(carts.router, prefix="/carts", tags=["cart"])
+
 
 
 @app.get("/")
